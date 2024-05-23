bd5629efa535bae8ad9b541363b3148ea1c73a98
timflee <timflee@hotmail.com>
2024-05-23 22:06:24 +1000

    minor tidy ups

--- main.ts
+++ main.ts
@@ -2,11 +2,11 @@ input.onButtonPressed(Button.A, function () {
     liveGraphMode = false
     eraseGraph()
     kitronik_air_quality.show("Temp - Replay Mode", 1, kitronik_air_quality.ShowAlign.Centre)
-    kitronik_air_quality.show("" + maxTempHistory, 2, kitronik_air_quality.ShowAlign.Right)
-    kitronik_air_quality.show("" + minTempHistory, 2, kitronik_air_quality.ShowAlign.Left)
+    kitronik_air_quality.show("", 2, kitronik_air_quality.ShowAlign.Right)
+    kitronik_air_quality.show("", 2, kitronik_air_quality.ShowAlign.Left)
     kitronik_air_quality.show(history[0][history[0].length - 1], 2, kitronik_air_quality.ShowAlign.Centre)
     for (let index = 0; index <= history[0].length - 1; index++) {
-        kitronik_air_quality.setPixel(index, Math.constrain(Math.map(history[0][index], minTempHistory, maxTempHistory, graphMinY, graphMaxY), graphMaxY, graphMinY))
+        kitronik_air_quality.setPixel(index, Math.constrain(Math.map(history[0][index], minGlobal, maxGlobal, graphMinY, graphMaxY), graphMaxY, graphMinY))
     }
 })
 function eraseGraph () {
@@ -18,11 +18,12 @@ input.onButtonPressed(Button.B, function () {
 // Toggle to the next mode
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     currentMode = modes[(modes.indexOf(currentMode) + 1) % modes.length]
-    basic.showString(currentMode)
+    basic.showString("" + (currentMode))
 })
 function initVariables () {
     modes = ["T", "P", "H"]
-    currentMode = "T"
+    currentMode = modes[0]
+    basic.showString("" + (currentMode))
     history = [[0], [0], [0]]
     play = [[0], [0], [0]]
     play[0].removeAt(0)
@@ -31,14 +32,10 @@ function initVariables () {
     liveGraphMode = true
     graphMaxY = 23
     graphMinY = 63
-    maxTempHistory = 0
-    minTempHistory = 100
-    maxTempGlobal = 15
-    minTempGlobal = 30
+    maxGlobal = 15
+    minGlobal = 30
 }
 let temp = 0
-let minTempGlobal = 0
-let maxTempGlobal = 0
 let heartBeat = false
 let maxHistoryLength = 0
 let play: number[][] = []
@@ -46,9 +43,9 @@ let modes: string[] = []
 let currentMode = ""
 let graphMaxY = 0
 let graphMinY = 0
+let maxGlobal = 0
+let minGlobal = 0
 let history: number[][] = []
-let minTempHistory = 0
-let maxTempHistory = 0
 let liveGraphMode = false
 let statusLEDs = kitronik_air_quality.createAirQualityZIPDisplay()
 statusLEDs.clear()
@@ -67,30 +64,18 @@ loops.everyInterval(100, function () {
     temp = kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)
     if (liveGraphMode) {
         kitronik_air_quality.show("Temp - Live Mode", 1, kitronik_air_quality.ShowAlign.Centre)
-        kitronik_air_quality.show("" + minTempGlobal, 2, kitronik_air_quality.ShowAlign.Left)
-        kitronik_air_quality.show("" + maxTempGlobal, 2, kitronik_air_quality.ShowAlign.Right)
+        kitronik_air_quality.show("" + minGlobal, 2, kitronik_air_quality.ShowAlign.Left)
+        kitronik_air_quality.show("" + maxGlobal, 2, kitronik_air_quality.ShowAlign.Right)
         kitronik_air_quality.show("" + temp, 2, kitronik_air_quality.ShowAlign.Centre)
         kitronik_air_quality.plot(Math.map(temp, 15, 30, 0, 100))
     }
-    if (temp > maxTempHistory) {
-        maxTempHistory = temp
+    if (temp > maxGlobal) {
+        maxGlobal = temp
     }
-    if (temp < minTempHistory) {
-        minTempHistory = temp
-    }
-    if (temp > maxTempGlobal) {
-        maxTempGlobal = temp
-    }
-    if (temp < minTempGlobal) {
-        minTempGlobal = temp
+    if (temp < minGlobal) {
+        minGlobal = temp
     }
     if (history[0].length == maxHistoryLength) {
-        if (history[0][0] == maxTempHistory) {
-            maxTempHistory = history[0][1]
-        }
-        if (history[0][0] == minTempHistory) {
-            minTempHistory = history[0][1]
-        }
         history[0].shift()
     }
     history[0].push(temp)
