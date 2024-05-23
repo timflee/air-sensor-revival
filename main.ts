ee43609a92d5cc9e8fe6222980b2054de2ed209a
timflee <timflee@hotmail.com>
2024-05-23 23:51:05 +1000

    All works pretty well. Not much point improving further right now. Probably best to switch to using the logging.

--- main.ts
+++ main.ts
@@ -1,29 +1,34 @@
 input.onButtonPressed(Button.A, function () {
     liveGraphMode = false
     kitronik_air_quality.clear()
-    kitronik_air_quality.refresh()
-    kitronik_air_quality.show("" + currentMode + " - Replay Mode", 1, kitronik_air_quality.ShowAlign.Left)
-    kitronik_air_quality.show(history[modes.indexOf(currentMode)].length, 1, kitronik_air_quality.ShowAlign.Right)
-    for (let index = 0; index <= history[modes.indexOf(currentMode)].length - 1; index++) {
-        kitronik_air_quality.setPixel(index, Math.constrain(Math.map(history[modes.indexOf(currentMode)][index], minGlobal[modes.indexOf(currentMode)], maxGlobal[modes.indexOf(currentMode)], graphMinY, graphMaxY), graphMaxY, graphMinY))
+    updateHeader()
+    for (let index = 0; index <= history[sensors.indexOf(currentSensor)].length - 1; index++) {
+        kitronik_air_quality.setPixel(index, Math.constrain(Math.map(history[sensors.indexOf(currentSensor)][index], minGlobal[sensors.indexOf(currentSensor)], maxGlobal[sensors.indexOf(currentSensor)], graphMinY, graphMaxY), graphMaxY, graphMinY))
     }
 })
-function eraseGraph () {
-    kitronik_air_quality.clear()
+function updateHeader () {
+    kitronik_air_quality.clearLine(1)
+    kitronik_air_quality.clearLine(2)
+    basic.showString("" + (currentSensor))
+    kitronik_air_quality.show(currentSensor, 1, kitronik_air_quality.ShowAlign.Left)
+    if (liveGraphMode) {
+        kitronik_air_quality.show("Live Mode", 1, kitronik_air_quality.ShowAlign.Centre)
+    } else {
+        kitronik_air_quality.show("Replay Mode", 1, kitronik_air_quality.ShowAlign.Centre)
+    }
 }
 input.onButtonPressed(Button.B, function () {
     liveGraphMode = !(liveGraphMode)
+    updateHeader()
 })
 // Toggle to the next mode
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    currentMode = modes[(modes.indexOf(currentMode) + 1) % modes.length]
-    kitronik_air_quality.clear()
-    basic.showString("" + (currentMode))
+    currentSensor = sensors[(sensors.indexOf(currentSensor) + 1) % sensors.length]
+    updateHeader()
 })
 function initVariables () {
-    modes = ["T", "H"]
-    currentMode = modes[0]
-    basic.showString("" + (currentMode))
+    sensors = ["T", "H"]
+    currentSensor = sensors[0]
     history = [[0], [0]]
     play = [[0], [0]]
     play[0].removeAt(0)
@@ -44,9 +49,9 @@ let graphMaxY = 0
 let graphMinY = 0
 let maxGlobal: number[] = []
 let minGlobal: number[] = []
-let modes: string[] = []
+let currentSensor = ""
+let sensors: string[] = []
 let history: number[][] = []
-let currentMode = ""
 let liveGraphMode = false
 let statusLEDs = kitronik_air_quality.createAirQualityZIPDisplay()
 statusLEDs.clear()
@@ -57,6 +62,7 @@ initVariables()
 statusLEDs.setZipLedColor(1, kitronik_air_quality.colors(ZipLedColors.Green))
 statusLEDs.show()
 kitronik_air_quality.clear()
+updateHeader()
 basic.forever(function () {
 	
 })
@@ -64,14 +70,13 @@ loops.everyInterval(200, function () {
     kitronik_air_quality.measureData()
     currentReading[0] = kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)
     currentReading[1] = kitronik_air_quality.readHumidity()
-    kitronik_air_quality.show(maxGlobal[modes.indexOf(currentMode)], 2, kitronik_air_quality.ShowAlign.Right)
-    kitronik_air_quality.show(minGlobal[modes.indexOf(currentMode)], 2, kitronik_air_quality.ShowAlign.Left)
-    kitronik_air_quality.show(currentReading[modes.indexOf(currentMode)], 2, kitronik_air_quality.ShowAlign.Centre)
+    kitronik_air_quality.show(maxGlobal[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Right)
+    kitronik_air_quality.show(minGlobal[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Left)
+    kitronik_air_quality.show(currentReading[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Centre)
     if (liveGraphMode) {
-        kitronik_air_quality.show("" + currentMode + " - Live Mode", 1, kitronik_air_quality.ShowAlign.Centre)
-        kitronik_air_quality.plot(Math.map(currentReading[modes.indexOf(currentMode)], 15, 30, 0, 100))
+        kitronik_air_quality.plot(Math.map(currentReading[sensors.indexOf(currentSensor)], 15, 30, 0, 100))
     }
-    for (let index = 0; index <= modes.length - 1; index++) {
+    for (let index = 0; index <= sensors.length - 1; index++) {
         if (currentReading[index] > maxGlobal[index]) {
             maxGlobal[index] = currentReading[index]
         }
