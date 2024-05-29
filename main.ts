1ba8dc32a332bdbd01f015d9173ce6fadbc20024
timflee <timflee@hotmail.com>
2024-05-29 23:20:05 +1000

    All working very well. Just need to add more live data and other metrics

--- main.ts
+++ main.ts
@@ -25,9 +25,7 @@ input.onButtonPressed(Button.A, function () {
     currentMode = "GraphMode"
     kitronik_air_quality.clear()
     updateHeader()
-    for (let index = 0; index <= history[sensors.indexOf(currentSensor)].length - 1; index++) {
-        kitronik_air_quality.setPixel(index, Math.constrain(Math.map(history[sensors.indexOf(currentSensor)][index], minHistory[sensors.indexOf(currentSensor)], maxHistory[sensors.indexOf(currentSensor)], graphMinY, graphMaxY), graphMaxY, graphMinY))
-    }
+    updateGraphMode()
 })
 function updateHeader () {
     kitronik_air_quality.clearLine(1)
@@ -45,19 +43,27 @@ input.onButtonPressed(Button.AB, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    kitronik_air_quality.clear()
     currentMode = "LiveMode"
+    kitronik_air_quality.clear()
     updateHeader()
 })
 function updateGraphMode () {
     kitronik_air_quality.show(maxHistory[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Right)
     kitronik_air_quality.show(minHistory[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Left)
     kitronik_air_quality.show(currentReadings[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Centre)
+    for (let index = 0; index <= history[sensors.indexOf(currentSensor)].length - 1; index++) {
+        tempY = Math.map(history[sensors.indexOf(currentSensor)][index], minHistory[sensors.indexOf(currentSensor)] * 0.999, maxHistory[sensors.indexOf(currentSensor)] * 1.001, graphMinY, graphMaxY)
+        kitronik_air_quality.setPixel(index, Math.constrain(tempY, graphMaxY, graphMinY))
+    }
 }
 // Toggle to the next mode
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    currentSensor = sensors[(sensors.indexOf(currentSensor) + 1) % sensors.length]
-    updateHeader()
+    if (currentMode == "GraphMode") {
+        currentSensor = sensors[(sensors.indexOf(currentSensor) + 1) % sensors.length]
+        kitronik_air_quality.clear()
+        updateHeader()
+        updateGraphMode()
+    }
 })
 function initVariables () {
     sensors = ["T", "H"]
@@ -71,26 +77,31 @@ function initVariables () {
     graphMaxY = 23
     graphMinY = 63
     maxHistory = [0, 0]
+    maxGlobal = [0, 0]
     minHistory = [100, 100]
+    minGlobal = [100, 100]
     currentReadings = [0, 0]
 }
 function updateLiveMode () {
-    kitronik_air_quality.show("Temp", 2, kitronik_air_quality.ShowAlign.Left)
-    kitronik_air_quality.show("RH", 3, kitronik_air_quality.ShowAlign.Left)
-    kitronik_air_quality.show("Pressure", 5, kitronik_air_quality.ShowAlign.Left)
+    kitronik_air_quality.show(maxGlobal[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Right)
+    kitronik_air_quality.show(minGlobal[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Left)
+    kitronik_air_quality.show(currentReadings[sensors.indexOf(currentSensor)], 2, kitronik_air_quality.ShowAlign.Centre)
 }
 let currentReading = 0
+let minGlobal: number[] = []
+let maxGlobal: number[] = []
 let heartBeat = false
 let maxHistoryLength = 0
 let play: number[][] = []
-let currentReadings: number[] = []
 let graphMaxY = 0
 let graphMinY = 0
-let maxHistory: number[] = []
-let minHistory: number[] = []
-let currentSensor = ""
-let sensors: string[] = []
+let tempY = 0
 let history: number[][] = []
+let currentReadings: number[] = []
+let minHistory: number[] = []
+let sensors: string[] = []
+let maxHistory: number[] = []
+let currentSensor = ""
 let currentMode = ""
 let temp = 0
 let bubblePop = false
@@ -113,23 +124,26 @@ statusLEDs.setZipLedColor(1, kitronik_air_quality.colors(ZipLedColors.Green))
 statusLEDs.show()
 kitronik_air_quality.clear()
 updateHeader()
-basic.forever(function () {
-	
-})
-loops.everyInterval(200, function () {
+loops.everyInterval(500, function () {
     kitronik_air_quality.measureData()
     currentReadings[0] = kitronik_air_quality.readTemperature(kitronik_air_quality.TemperatureUnitList.C)
     currentReadings[1] = kitronik_air_quality.readHumidity()
     if (currentMode == "GraphMode") {
-        updateGraphMode()
+    	
     }
     if (currentMode == "LiveMode") {
         updateLiveMode()
     }
     for (let index = 0; index <= sensors.length - 1; index++) {
         currentReading = currentReadings[index]
+        if (currentReading > maxGlobal[index]) {
+            maxGlobal[index] = currentReading
+        }
         if (currentReading > maxHistory[index]) {
             maxHistory[index] = currentReading
+        }
+        if (currentReading < minGlobal[index]) {
+            minGlobal[index] = currentReading
         }
         if (currentReading < minHistory[index]) {
             minHistory[index] = currentReading
@@ -152,4 +166,7 @@ loops.everyInterval(200, function () {
     }
     statusLEDs.show()
     heartBeat = !(heartBeat)
+})
+basic.forever(function () {
+	
 })
